a725f50e7a021b3fd208f0d57d2dcb69d5d2ad58
Jeanne <tian.jeanne@gmail.com>
2024-09-03 12:26:49 +0200

    Create the user modeling
    I created the User model with email and password_digest attributes.
    
    Email should be unique so I enforced it at database level with a unique
    index and in the model validations.
    
    Bcrypt gem was added to the Gemfile to be able to use
    "has_secure_password" https://guides.rubyonrails.org/active_model_basics.html#securepassword

---
+++ db/migrate/20240903092053_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, index: { unique: true }
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
