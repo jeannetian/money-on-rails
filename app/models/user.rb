c22796cf5ffba0bee9212deec71166735431957a
Jeanne <tian.jeanne@gmail.com>
2024-09-09 15:00:37 +0200

    CRUD on Transaction model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :sessions, dependent: :destroy
   has_many :bank_accounts, dependent: :destroy
+  has_many :transactions, through: :bank_accounts
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
