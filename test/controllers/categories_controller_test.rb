a49701cb285f7639a527bbd87509aa7949126794
Jeanne <tian.jeanne@gmail.com>
2024-09-17 16:54:33 +0200

    Add tests for CategoriesController

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:jeanne)
+  end
+
+  test "#index" do
+    get categories_path
+    assert_response :success
+  end
+
+  test "#show" do
+    get category_path categories(:home)
+    assert_response :success
+  end
+
+  test "#new" do
+    get new_category_path
+    assert_response :success
+  end
+
+  test "#create" do
+    assert_difference -> { users(:jeanne).categories.count }, 1 do
+      post categories_path, params: { category: { name: "New category" } }
+    end
+
+    assert_redirected_to categories_path
+  end
+
+  test "#edit" do
+    get edit_category_path categories(:home)
+    assert_response :success
+  end
+
+  test "#update" do
+    assert_changes -> { categories(:home).reload.name }, to: "Updated name" do
+      patch category_path categories(:home), params: { category: { name: "Updated name" } }
+    end
+
+    assert_redirected_to categories_path
+  end
+
+  test "#destroy" do
+    assert_difference -> { users(:jeanne).categories.count }, -1 do
+      delete category_path(categories(:home))
+    end
+
+    assert_redirected_to categories_path
+  end
 end
