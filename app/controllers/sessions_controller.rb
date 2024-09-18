685ff44b1152b5f29fa24055ad498288950974f4
Jeanne <tian.jeanne@gmail.com>
2024-09-18 12:22:30 +0200

    Design session page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
       flash[:alert] = "Invalid email or password"
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
