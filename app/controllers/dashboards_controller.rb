689876463a0de284d7247aeaad56d86fe31de17b
Jeanne <tian.jeanne@gmail.com>
2024-10-14 11:02:20 +0200

    Add dashboard calculations

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class DashboardsController < ApplicationController
   def show
+    @bank_accounts = Current.user.bank_accounts.order(:name)
+    @transactions = Current.user.transactions.order(:date)
+    @total_by_category = Current.user.transactions.left_joins(:category).group(:category).sum(:amount)
   end
 end
